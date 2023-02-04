0a5323f4df7b664989d2558078e7bc62fce21a4f
ulja4a <ulja4a@gmail.com>
2023-02-04 21:44:31 +0400

    feat: add accordion to section price

--- plants/index.js
+++ plants/index.js
@@ -28,8 +28,11 @@
     const blurItemGarden = document.querySelectorAll(".gardens");
     const blurItemSecondGarden = document.querySelectorAll("service-item-second.gardens");
     const blurItemLawn = document.querySelectorAll(".lawn");
-
-    
+    /*let checkerG = false;
+    servicesButtonFirst.onclick = () => checkerG = true;
+    if (checkerG === true){
+        console.log('check');
+    } else {*/
      servicesButtonFirst.addEventListener("click", (event) => {
         servicesButtonFirst.classList.toggle("services-button_activ");
         blurItemPlants.forEach((el) => {
@@ -73,3 +76,55 @@
     })
 
 }());
+
+//accordion
+(function () {
+    const AccordionItem = document.querySelectorAll(".accordion_header")
+    const AccordionItem2 = document.querySelectorAll(".accordion_header2")
+    const AccordionItem3 = document.querySelectorAll(".accordion_header3")
+    const AccordionBody = document.querySelector(".price_item1")
+    const AccordionBody2 = document.querySelector(".price_item2")
+    const AccordionBody3 = document.querySelector(".price_item3")
+    AccordionItem.forEach((el) => {
+        el.addEventListener("click", (event) => {
+    
+
+    AccordionBody.classList.toggle("accordion_item_show")
+    AccordionBody2.classList.remove("accordion_item_show")
+    AccordionBody3.classList.remove("accordion_item_show")
+    })
+    })
+
+    AccordionItem2.forEach((el) => {
+        el.addEventListener("click", (event) => {
+    
+
+    AccordionBody2.classList.toggle("accordion_item_show")
+    AccordionBody.classList.remove("accordion_item_show")
+    AccordionBody3.classList.remove("accordion_item_show")
+    })
+    })
+
+    AccordionItem3.forEach((el) => {
+        el.addEventListener("click", (event) => {
+    
+
+    AccordionBody3.classList.toggle("accordion_item_show")
+    AccordionBody.classList.remove("accordion_item_show")
+    AccordionBody2.classList.remove("accordion_item_show")
+    })
+    })
+
+}());
+
+/*(function () {
+    document.querySelector(".price_item1").classList.add("accordion_item_show")
+    document.querySelectorAll(".price_item1").forEach((sec) =>{
+        sec.addEventListener("click", (event) => {
+            console.log(1)
+                document.querySelectorAll(".price_item1").forEach((el) => {
+                    el.classList.removed("accordion_item_show")
+                })
+        })
+    })
+})*/
