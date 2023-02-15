95d3db206accaf6bf7315cbc721deebad5a304bf
ulja4a <ulja4a@gmail.com>
2023-02-15 20:47:14 +0400

    feat: add local time

---
+++ momentum/js/script.js
@@ -0,0 +1,11 @@
+    const time = document.querySelector(".time");
+    
+    
+function showTime() {
+    const date = new Date();
+    time.textContent = date.toLocaleTimeString(); 
+    setTimeout(showTime, 1000);  
+  }
+  showTime();
+  
+
