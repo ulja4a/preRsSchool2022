5d8c12608372ae075264828b61b27b0fbf9b9bd7
ulja4a <ulja4a@gmail.com>
2023-02-15 23:23:59 +0400

    feat: add date and greeting

--- momentum/js/script.js
+++ momentum/js/script.js
@@ -1,11 +1,56 @@
     const time = document.querySelector(".time");
+    const currentDate = document.querySelector(".date")
+    const greeting = document.querySelector(".greeting")
+    const date = new Date();
     
+    function getTimeOfDay(){
+        const hours = date.getHours();    
+        if (hours>0 && hours<6) {
+            return "night";
+        } 
+        if (hours>=6 && hours<12) {
+            return "morning"; 
+        }
+        if (hours>=12 && hours<18) {
+            return "afternoon";
+        } else {
+            return "evening";
+        }
+        
+    }
     
 function showTime() {
     const date = new Date();
+    const hours = date.getHours();
+    console.log(hours)
+    
+    
     time.textContent = date.toLocaleTimeString(); 
-    setTimeout(showTime, 1000);  
-  }
+    setTimeout(showTime, 1000); 
+        function showDate() {
+            const options = {weekday: 'long', month: 'long', day: 'numeric'};
+            currentDate.textContent = date.toLocaleDateString('en-Us', options);   
+        }
+        showDate(); 
+        
+            
+        function showGreeting() {
+            const timeOfDay = getTimeOfDay();
+            const greetingText = `Good ${timeOfDay}`;
+            
+            greeting.textContent = greetingText
+        }
+        showGreeting()     
+        
+}
   showTime();
+
   
+
+  
+
+
+
+  
+
 
