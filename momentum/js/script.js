261e348f7a8e041b665a0a7566d54ecd7f773449
ulja4a <ulja4a@gmail.com>
2023-02-22 19:08:35 +0400

    feat: add greeting name

--- momentum/js/script.js
+++ momentum/js/script.js
@@ -1,7 +1,8 @@
-    const time = document.querySelector(".time");
+const time = document.querySelector(".time");
     const currentDate = document.querySelector(".date")
     const greeting = document.querySelector(".greeting")
     const date = new Date();
+    body.style.backgroundImage = "url('https://raw.githubusercontent.com/rolling-scopes-school/stage1-tasks/assets/images/evening/18.jpg')";
     
     function getTimeOfDay(){
         const hours = date.getHours();    
@@ -22,8 +23,7 @@
 function showTime() {
     const date = new Date();
     const hours = date.getHours();
-    console.log(hours)
-    
+    let name = document.querySelector('input.name');
     
     time.textContent = date.toLocaleTimeString(); 
     setTimeout(showTime, 1000); 
@@ -39,18 +39,23 @@ function showTime() {
             const greetingText = `Good ${timeOfDay}`;
             
             greeting.textContent = greetingText
+
+            function setLocalStorage() {
+                
+                localStorage.setItem('name', name.value);
+              }
+              window.addEventListener('beforeunload', setLocalStorage)
+              
+              function getLocalStorage() {
+                if(localStorage.getItem('name')) {
+                    
+                  name.value = localStorage.getItem('name');
+                }
+              }
+              window.addEventListener('load', getLocalStorage)
         }
         showGreeting()     
         
 }
   showTime();
-
   
-
-  
-
-
-
-  
-
-
